af7a0194c232507dff474dbc14ec6e4b40815fdc
Lindsay Johnston <lindsaykjohnston@outlook.com>
2020-11-18 12:26:00 -0600

    Final project with updated sass

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss';
+import Menu from './Menu/Menu';
+import Home from './Home/Home';
+import Projects from './Projects/Projects';
+import About from './About/About';
+import Contact from './Contact/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+       <Menu />
+       <Home />
+       <Projects />
+       <About />
+       <Contact />
     </div>
   );
 }
