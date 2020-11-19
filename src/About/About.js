33edf528b4bd0058400b24041a1d2328d613cc17
Lindsay Johnston <lindsaykjohnston@outlook.com>
2020-11-19 13:28:34 -0600

    Tweak resume

--- src/About/About.js
+++ src/About/About.js
@@ -10,7 +10,7 @@ const About= (props) =>(
             <img src={lindsayGuitar} alt="Lindsay with guitar"></img>
             <p>My work experiences have varied greatly: from the non-profit sector to the service industry, from education to my most current role as creative director for a rock-and-roll band. The common threads that weave through all of my professional experiences are creativity and innovation. Whether I am creating a teaching strategy, an app or a t-shirt design, I am always looking to improve upon prior work and present the project in an engaging and professional way. I strive to communicate directly and honestly, work efficiently, and push for female representation in historically male-dominated fields.</p>
   
-            <a href={lindsayResume} target="blank">Resume</a>
+            <a href={lindsayResume} target="_blank">Resume</a>
 
             
         </div>
