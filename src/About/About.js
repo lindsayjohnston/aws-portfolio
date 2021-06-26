8529bb365805086e1b1537b82c174276e0f2cb2d
Lindsay Johnston <lindsaykjohnston@outlook.com>
2021-06-25 22:16:44 -0500

    Updated resume link

--- src/About/About.js
+++ src/About/About.js
@@ -9,9 +9,9 @@ const About= (props) =>(
         <div className={styles.AboutContent}>
             
             <img src={lindsayGuitar} alt="Lindsay with guitar"></img>
-            <p>My work experiences have varied greatly: from the non-profit sector to the service industry, from education to my most current role as creative director for a rock-and-roll band. The common threads that weave through all of my professional experiences are creativity and innovation. Whether I am creating a teaching strategy, an app or a t-shirt design, I am always looking to improve upon prior work and present the project in an engaging and professional way. I strive to communicate directly and honestly, work efficiently, and push for female representation in historically male-dominated fields.</p>
+            <p>My work experiences have varied greatly: from the non-profit sector to the service industry, from education to creative director for a rock-and-roll band. The common threads that weave through all of my professional experiences are creativity and innovation. Whether I am creating a teaching strategy, an app or a t-shirt design, I am always looking to improve upon prior work and present the project in an engaging and professional way. I strive to communicate directly and honestly, work efficiently, and push for female representation in historically male-dominated fields.</p>
   
-            <h1 className={styles.ResumeLink}><a href={"https://lindsayjohnston.github.io/Portfolio-2020/Lindsay_Johnston_WD_resume_2020.pdf"} target="_blank" rel="noreferrer">
+            <h1 className={styles.ResumeLink}><a href={"https://lindsayjohnston.github.io/Portfolio-2020/Lindsay_Johnston_web_developer_resume_June_2021.pdf"} target="_blank" rel="noreferrer">
             Resume</a> </h1>
 
             
